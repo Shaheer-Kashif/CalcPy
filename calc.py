0ecc08bb01b82efa5f0364807230572f02ed7e5f
Shaheer <shaheerkashif345@gmail.com>
2024-01-29 12:24:33 +0500

    Fixed Equal to Functionality and calculator now mimicks more closely to the windows calculator, icon added

--- calc.py
+++ calc.py
@@ -4,16 +4,23 @@ num = 0
 num2 = 0
 sign = None
 count = 0
+pas = True
+
 root = Tk()
 root.title("Calculator by Shaheer Kashif")
+root.iconbitmap("ico/calc.ico")
 
 def buttonclick(inp):
+    global count,pas
+    if count >= 2 and pas == 0:
+        e.delete(0,END)
+        pas = 1
     additional = e.get()
     e.delete(0,END)
     e.insert(0,additional+inp)
     
 def buttonoperator(op):
-    global num,num2,sign,count
+    global num,num2,sign,count,pas
     if op == "+" or op == "-" or op == "*" or op == "÷":
         count += 1
         if count >= 2:
@@ -26,6 +33,8 @@ def buttonoperator(op):
                 result = int(num) * int(num2)
             elif sign == "÷":
                 result = int(num) / int(num2)
+            num = result
+            pas = 0
             sign = op
             e.delete(0,END)
             e.insert(0,result)
@@ -35,24 +44,21 @@ def buttonoperator(op):
             e.delete(0,END)
         
     elif op == "=":
+        count = 0
         num2 = e.get()
-        if op == "+":
+        if sign == "+":
             result = int(num) + int(num2)
-        elif op == "-":
+        elif sign == "-":
             result = int(num) - int(num2)
-        elif op == "*":
+        elif sign == "*":
             result = int(num) * int(num2)
-        elif op == "÷":
+        elif sign == "÷":
             result = int(num) / int(num2)
         e.delete(0,END)
         e.insert(0,result)
 
 def buttonclear():
     e.delete(0,END)
-    
-
-def buttonequalto():
-    pass
 
 e = Entry(root, width=40, borderwidth=10)
 e.grid(row=0,column=0,columnspan=4)
@@ -70,14 +76,14 @@ button_7 = Button(root,text = "7",command = lambda: buttonclick('7'),padx = 28,p
 button_8 = Button(root,text = "8",command = lambda: buttonclick('8'),padx = 28,pady = 14)
 button_9 = Button(root,text = "9",command = lambda: buttonclick('9'),padx = 28,pady = 14)
 
-button_0 = Button(root,text = "0",command = lambda: buttonclick(),padx = 28,pady = 14)
-button_clear = Button(root,text = "C",command = buttonclear,padx = 28,pady = 14)
+button_0 = Button(root,text = "0",command = lambda: buttonclick('0'),padx = 28,pady = 14)
+button_clear = Button(root,text = "C",command = buttonclear,padx = 27,pady = 14)
 
-button_add = Button(root,text = "+",command = lambda: buttonoperator('+'),padx = 28,pady = 14)
-button_subtract = Button(root,text = "-",command = lambda: buttonoperator('-'),padx = 28,pady = 14)
+button_add = Button(root,text = "+",command = lambda: buttonoperator('+'),padx = 27,pady = 14)
+button_subtract = Button(root,text = "-",command = lambda: buttonoperator('-'),padx = 29,pady = 14)
 button_multiply = Button(root,text = "x",command = lambda: buttonoperator('*'),padx = 28,pady = 14)
-button_divide = Button(root,text = "÷",command = lambda: buttonoperator('÷'),padx = 28,pady = 14)
-button_equal = Button(root,text = "=",command = lambda: buttonoperator('='),padx = 28,pady = 14)
+button_divide = Button(root,text = "÷",command = lambda: buttonoperator('÷'),padx = 27,pady = 14)
+button_equal = Button(root,text = "=",command = lambda: buttonoperator('='),padx = 27,pady = 14)
 
 # placing buttons
 button_1.grid(row = 3,column = 0)
