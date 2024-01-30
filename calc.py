e01439e77b0449202ca156eaa870b063bdd518a2
Shaheer <shaheerkashif345@gmail.com>
2024-01-30 12:49:21 +0500

    History Label Added, Functionality Remaining

--- calc.py
+++ calc.py
@@ -65,8 +65,10 @@ def buttonoperator(op):
 def buttonclear():
     e.delete(0,END)
 
+history_label = Label(root, text= "History here")
+history_label.grid(row = 0 , column = 0)
 e = Entry(root, width=40, borderwidth=10,state="normal")
-e.grid(row=0,column=0,columnspan=4)
+e.grid(row=1,column=0,columnspan=4)
 
 # defining buttons
 button_1 = Button(root,text = "1",command = lambda: buttonclick('1'),padx = 28,pady = 14)
@@ -91,26 +93,26 @@ button_divide = Button(root,text = "÷",command = lambda: buttonoperator('÷'),p
 button_equal = Button(root,text = "=",command = lambda: buttonoperator('='),padx = 27,pady = 14)
 
 # placing buttons
-button_1.grid(row = 3,column = 0)
-button_2.grid(row = 3,column = 1)
-button_3.grid(row = 3,column = 2)
+button_1.grid(row = 4,column = 0)
+button_2.grid(row = 4,column = 1)
+button_3.grid(row = 4,column = 2)
 
-button_4.grid(row = 2,column = 0)
-button_5.grid(row = 2,column = 1)
-button_6.grid(row = 2,column = 2)
+button_4.grid(row = 3,column = 0)
+button_5.grid(row = 3,column = 1)
+button_6.grid(row = 3,column = 2)
 
-button_7.grid(row = 1,column = 0)
-button_8.grid(row = 1,column = 1)
-button_9.grid(row = 1,column = 2)
+button_7.grid(row = 2,column = 0)
+button_8.grid(row = 2,column = 1)
+button_9.grid(row = 2,column = 2)
 
-button_0.grid(row = 4,column = 0)
-button_clear.grid(row = 4,column = 1)
-button_equal.grid(row = 4,column = 2)
+button_0.grid(row = 5,column = 0)
+button_clear.grid(row = 5,column = 1)
+button_equal.grid(row = 5,column = 2)
 
-button_add.grid(row = 1,column = 3)
-button_subtract.grid(row = 2,column = 3)
-button_multiply.grid(row = 3,column = 3)
-button_divide.grid(row = 4,column = 3)
+button_add.grid(row = 2,column = 3)
+button_subtract.grid(row = 3,column = 3)
+button_multiply.grid(row = 4,column = 3)
+button_divide.grid(row = 5,column = 3)
 
 # Event binding to ignore keyboard input
 e.bind("<Key>", ignore_keyboard_input)
