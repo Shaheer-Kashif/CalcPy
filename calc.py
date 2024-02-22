354b4f5539079174365aabe041692c0cf298fdc9
Shaheer <shaheerkashif345@gmail.com>
2024-02-22 12:07:32 +0500

    Bugs Fixed Regarding Trignometric Functions and 1-9 Buttons are created and placed using loop

--- calc.py
+++ calc.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from math import *
 
+#Main Variables
 num = 0
 num2 = 0
 sign = None
@@ -10,9 +11,11 @@ hist = ""
 result = 0
 oneoff = 0
 
+#Main Window
 root = Tk()
 root.title("Calculator by Shaheer Kashif")
 root.iconbitmap("ico/calc.ico")
+root.resizable(False, False)
 
 def ignore_keyboard_input(event):
     return "break"
@@ -47,7 +50,6 @@ def buttonoperator(op):
                     hist = history_label.get().replace("",history_label.get()[0:history_label.get().index("√")+1])
                 elif "ᛑ/"in history_label.get():
                     hist = history_label.get().replace("",history_label.get()[0:history_label.get().index("ᛑ/")])
-                    
             history_label.delete(0,END)
         if oneoff == 0:
             hist = e.get() + op
@@ -137,7 +139,7 @@ def oneoffs(operation):
     if e.get() == "":
         pass
     else:
-        if sign != "":
+        if sign != None:
             global num2,count,oneoff
             if operation == '!':
                 num2 = e.get()
@@ -211,8 +213,9 @@ def percent():
         e.delete(0,END)
         e.insert(0,tempnum)
         
+#Trigonometric Function Button
 def trigno_funcs(*args):
-    global sign,count
+    global sign,count,oneoff
     trig_option = trig_val.get()
     trig_val.set("Trignometric Functions")
     trigfunc = OptionMenu(root, trig_val, *trig_options)
@@ -233,18 +236,27 @@ def trigno_funcs(*args):
         e.delete(0,END)
         count+=1
         e.insert(0,tempnum)
-        if sign != "":
+        if sign != None:
             pass
+            oneoff = 1
         else:
             sign = "trig"
+            
+trig_val = StringVar()
+trig_val.set("Trignometric Functions")
+trig_options = ["sin","cos","tan"]
+trigfunc = OptionMenu(root, trig_val, *trig_options)
+trig_val.trace("w",trigno_funcs)
+trigfunc.grid(row=2,column=0,columnspan=2)
 
+#Trigonometric Inverse Function Button
 def trigno_funcs_inverse(*args):
-    global sign,count
+    global sign,count,oneoff
     trig_option2 = trig_val2.get()
-    trig_val2.set("Trignometric Functions Inverse")
-    trigfunc2 = OptionMenu(root, trig_val, *trig_options2)
+    trig_val2.set("Trignometric Inverse Functions")
+    trigfunc2 = OptionMenu(root, trig_val2, *trig_options2)
     trigfunc2.grid(row=2,column=2,columnspan=3,ipadx=17)
-    if trig_option2 == "Trignometric Functions Inverse" or e.get() == "":
+    if trig_option2 == "Trignometric Inverse Functions" or e.get() == "":
         pass
     else:
         tempnum = e.get()
@@ -260,30 +272,32 @@ def trigno_funcs_inverse(*args):
         e.delete(0,END)
         e.insert(0,tempnum)
         count += 1
-        if sign != "":
+        if sign != None:
             pass
+            oneoff = 1
         else:
             sign = "trig_inv"
+            
+trig_val2 = StringVar()
+trig_val2.set("Trignometric Inverse Functions")
+trig_options2 = ["sin⁻¹","cos⁻¹","tan⁻¹"]
+trigfunc2 = OptionMenu(root, trig_val2, *trig_options2)
+trig_val2.trace("w",trigno_funcs_inverse)
+trigfunc2.grid(row=2,column=2,columnspan=3,ipadx=17)
         
-history_label = Entry(root,width=63,justify="right",borderwidth=0,fg="#3A3A3A")
+#History and Calculation Label
+history_label = Entry(root,width=68,justify="right",borderwidth=0,fg="#3A3A3A")
 history_label.grid(row = 0,column=0,columnspan=5)
-e = Entry(root, width=25,state="normal",font=("Helvetica",20),justify="right",borderwidth=0)
+e = Entry(root, width=27,state="normal",font=("Helvetica",20),justify="right",borderwidth=0)
 e.grid(row=1,column=0,columnspan=5)
 
 # defining buttons
-button_1 = Button(root,text = "1",command = lambda: buttonclick('1'),padx = 32,pady = 14,font= "helvetica",bg="white")
-button_2 = Button(root,text = "2",command = lambda: buttonclick('2'),padx = 28,pady = 14,font= "helvetica",bg="white")
-button_3 = Button(root,text = "3",command = lambda: buttonclick('3'),padx = 28,pady = 14,font= "helvetica",bg="white")
+for i in range(0,10):
+    if i == 1 or i == 4 or i == 7:
+        locals()["button_"+str(i)] = Button(root,text = str(i) ,command = lambda x = i: buttonclick(str(x)),padx = 32,pady = 14,font= "helvetica",bg="white")
+    else:
+        locals()["button_"+str(i)] = Button(root,text = str(i),command = lambda x = i: buttonclick(str(x)),padx = 28,pady = 14,font= "helvetica",bg="white")
 
-button_4 = Button(root,text = "4",command = lambda: buttonclick('4'),padx = 32,pady = 14,font= "helvetica",bg="white")
-button_5 = Button(root,text = "5",command = lambda: buttonclick('5'),padx = 28,pady = 14,font= "helvetica",bg="white")
-button_6 = Button(root,text = "6",command = lambda: buttonclick('6'),padx = 28,pady = 14,font= "helvetica",bg="white")
-
-button_7 = Button(root,text = "7",command = lambda: buttonclick('7'),padx = 32,pady = 14,font= "helvetica",bg="white")
-button_8 = Button(root,text = "8",command = lambda: buttonclick('8'),padx = 28,pady = 14,font= "helvetica",bg="white")
-button_9 = Button(root,text = "9",command = lambda: buttonclick('9'),padx = 28,pady = 14,font= "helvetica",bg="white")
-
-button_0 = Button(root,text = "0",command = lambda: buttonclick('0'),padx = 28,pady = 14,font= "helvetica",bg="white")
 button_decimal = Button(root,text = ".",command = buttondecimal,padx = 31,pady = 14,font= "helvetica",bg="white")
 button_plusminus = Button(root,text = "+/-",command = plusminus,padx = 27,pady = 14,font= "helvetica",bg="white")
 button_clear = Button(root,text = "C",command = buttonclear,padx = 29,pady = 14,font= "helvetica")
@@ -301,36 +315,16 @@ button_squareroot = Button(root,text = "√x",command = lambda: oneoffs('root'),
 
 button_percent = Button(root,text = "%",command = percent,padx = 28,pady = 14,font= "helvetica")
 button_reciprocal = Button(root,text = "1/x",command = lambda: oneoffs('reciprocal'),padx = 23,pady = 14,font= "helvetica")
-
-trig_val = StringVar()
-trig_val.set("Trignometric Functions")
-trig_options = ["sin","cos","tan"]
-trigfunc = OptionMenu(root, trig_val, *trig_options)
-trig_val.trace("w",trigno_funcs)
-trigfunc.grid(row=2,column=0,columnspan=2)
-
-trig_val2 = StringVar()
-trig_val2.set("Trignometric Functions Inverse")
-trig_options2 = ["sin⁻¹","cos⁻¹","tan⁻¹"]
-trigfunc2 = OptionMenu(root, trig_val2, *trig_options2)
-trig_val2.trace("w",trigno_funcs_inverse)
-trigfunc2.grid(row=2,column=2,columnspan=3,ipadx=17)
-
-                        
+    
 # placing buttons
-button_1.grid(row = 6,column = 0)
-button_2.grid(row = 6,column = 1)
-button_3.grid(row = 6,column = 2)
-
-button_4.grid(row = 5,column = 0)
-button_5.grid(row = 5,column = 1)
-button_6.grid(row = 5,column = 2)
-
-button_7.grid(row = 4,column = 0)
-button_8.grid(row = 4,column = 1)
-button_9.grid(row = 4,column = 2)
+key = 1
+for rows in range(6,3,-1):
+    for col in range(3):
+        (locals()["button_"+str(key)]).grid(row = rows,column = col)
+        key += 1
 
 button_0.grid(row = 7,column = 1)
+
 button_plusminus.grid(row=7,column=0)
 button_decimal.grid(row = 7,column = 2)
 button_clear.grid(row = 4,column = 4)
@@ -350,8 +344,6 @@ button_divide.grid(row = 6,column = 3)
 button_equal.grid(row = 7,column=3)
 button_exponent.grid(row = 6,column=4)
 button_factorial.grid(row = 7,column=4)
-
-root.resizable(False, False)
 
 # Event binding to ignore keyboard input
 e.bind("<Key>", ignore_keyboard_input)
